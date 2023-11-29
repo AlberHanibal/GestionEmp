00f71895d7a00ea12441df9615fdd072edcb03d6
Alber clase <alber@jovellanos>
2023-11-29 11:32:01 +0100

    fin

--- Tema 3/BDBiblioteca/biblioteca.py
+++ Tema 3/BDBiblioteca/biblioteca.py
@@ -2,44 +2,63 @@ import pyodbc
 from pyodbc import connect
 cnxn = 0
 
+# falta la funcion formatear
+
+
 def conectar():
     server = 'C13-PC16\SQLEXPRESS01'
     database = 'LIBRERIA'
     username = 'librero'
     password = 'librero'
     cnxn = pyodbc.connect(
-    'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';ENCRYPT=no;UID=' + username + ';PWD=' + password)
+        'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';ENCRYPT=no;UID=' + username + ';PWD=' + password)
     return cnxn
+
 
 def desconectar(cnxn):
     cnxn.close()
 
 
-def añadirLibro():
+def añadirLibro(titulo, isbn, annoPublicacion, autor, editorial, genero, stock, idioma):
     cnxn = conectar()
     cursor = cnxn.cursor()
+    cursor.execute("exec [biblio].[buscarNombreAutor] ?", autor)
+    idAutor = cursor.fetchone()
+    cursor.execute("exec [biblio].[añadirLibro] ?, ?, ?, ?, ?, ? ,?, ?", titulo,
+                   isbn, annoPublicacion, idAutor[0], editorial, genero, stock, idioma)
+    cursor.commit()
+    desconectar(cnxn)
 
-    desconectar()
+
+def añadirAutor(nombre, apellido, fechaNac, nacionalidad):
+    cnxn = conectar()
+    cursor = cnxn.cursor()
+    cursor.execute("exec [biblio].[añadirAutor] ?, ?, ?, ?",
+                   nombre, apellido, fechaNac, nacionalidad)
+    cursor.commit()
+    desconectar(cnxn)
+
+
+def comprobarAutor(autor):
+    cnxn = conectar()
+    cursor = cnxn.cursor()
+    cursor.execute("exec [biblio].[buscarNombreAutor] ?", autor)
+    row = cursor.fetchone()
+    if (row is not None):
+        desconectar(cnxn)
+        return True
+    desconectar(cnxn)
+    return False
+
 
 def pedirDigito():
-    comprobarDigito = False;
+    comprobarDigito = False
     while (not comprobarDigito):
         num = input()
         comprobarDigito = num.isdigit()
         if (not comprobarDigito):
             print("Tienes que introducir un número")
     return int(num)
-
-def comprobarAutor(autor):
-    cnxn = conectar()
-    cursor = cnxn.cursor()
-    cursor.execute("exec [biblio].[buscarNombreAutor] \'" + autor + "\'")
-    # hay que usar row no se bien como
-    if (cursor.rowcount) > 0:
-        desconectar(cnxn)
-        return True
-    desconectar(cnxn)
-    return False
 
 
 opcion = 0
@@ -69,8 +88,27 @@ while (not opcion == 5):
             autor = input()
             if comprobarAutor(autor):
                 print("El autor existe")
-            
-
-
-
-
+            else:
+                print("No tenemos a ese autor. Vamos a añadirlo.")
+                print("Dame su nombre")
+                nombre = input()
+                print("Dame su apellido")
+                apellido = input()
+                print("Dame su fecha de nacimiento (YYYY/MM/DD)")
+                fechaNac = input()
+                # comprobar fecha
+                print("Dame su nacionalidad")
+                nacionalidad = input()
+                añadirAutor(nombre, apellido, fechaNac, nacionalidad)
+                autor = nombre + " " + apellido
+                print("Autor añadido.")
+            print("Dame la editorial")
+            editorial = input()
+            print("Dame el genero")
+            genero = input()
+            print("Dame el stock")
+            stock = pedirDigito()
+            print("Dame el idioma")
+            idioma = input()
+            añadirLibro(titulo, isbn, annoPublicacion, autor,
+                        editorial, genero, stock, idioma)
